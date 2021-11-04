e77eff4e63d643fe53de54f806ff48a26e1bda17
RiazK <RiazCKhan@gmail.com>
2021-11-04 18:17:01 -0400

    Import Helpers FNs

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,6 +1,16 @@
 import Player from "./Player";
+import matchData from "../data/matchData";
+import playerData from "../data/playerData";
+import { addWinsToPlayers, preparePlayerdata } from "../helpers/playerHelpers";
 
-const PlayerList = () => {
+
+const PlayerList = (props) => {
+  const playerDataArray = preparePlayerdata(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
+  console.log(playerDataArray)
+  console.log('parsed', parsedPlayerData)
+  
   return (
     <section className="PlayerList">
     <h1>Current participating players</h1>
