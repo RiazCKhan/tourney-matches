70f2f74a8071b25deeeae5f6654f848c10256109
RiazK <RiazCKhan@gmail.com>
2021-11-05 16:39:53 -0400

    Render all matches

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -1,13 +1,29 @@
+import matchData from "../data/matchData";
 import Match from "./Match";
 
 const MatchList = (props) => {
 
-  const oneMatch = props.matchData[0];
-  
-  return (
+const matches = props.matchData.map((match) => {
+
+// console.log('props match data', props.matchData)
+// console.log('i am each match', match)
+
+return (
+  <Match
+  key={ match.matchNumber }
+  players={ match.players }
+  winner={ match.winner }
+  scoreDifference={ match.scoreDifference } 
+  />
+  )
+})
+
+// console.log('i am matches', matches)
+
+return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match player={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
+      {matches}
     </section>
   );
 }
