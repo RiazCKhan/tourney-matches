8772b683c9e79eb0eb7ec0cd7680b6ed83013f9e
RiazK <RiazCKhan@gmail.com>
2021-11-05 10:19:22 -0400

    Add conditional rendering

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,11 +1,25 @@
-const Match = () => {
+const Match = (props) => {
+
+  //console.log('match prop', props)
+
   return (
     <article className="Match">
-      <h1>Joe <span>vs</span> Bob</h1>
-      {/* To be shown when there is a winner */}<h2>Joe is the winner by 1!</h2>
-      {/* To be shown when there is no winner */}<h2>No winners yet!</h2>
+      <h1> {props.player[0]} <span>vs</span> {props.player[1]} </h1>
+      {(props.winner) ? <h2> {props.winner} is the winner by {props.scoreDifference}!</h2> : <h2>No winners yet!</h2>}
     </article>
   )
 }
+
+
+/* Alternative solutions for/from Condition Rendering 'Walk Thru' 
+  {props.winner && <h2>{props.winner} is the winner by {props.scoreDifference}!</h2>}
+  {!props.winner && <h2>No winners yet!</h2>}
+
+  !props.winner
+  - if no winner, i.e., input = undefined/null = falsy
+  - !props.winner = !falsy = True, render 'No winners'
+  OR
+  - if winner, i.e., input = truthy
+  - !props.winner = !truthy = False, WILL NOT render 'No winners' */
 
 export default Match;
