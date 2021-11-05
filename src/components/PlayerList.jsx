572226c152f4543e359dd48131e2311ed542dacf
RiazK <RiazCKhan@gmail.com>
2021-11-05 11:23:54 -0400

    Receive and implement prop from App.js

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,21 +1,16 @@
 import Player from "./Player";
-import matchData from "../data/matchData";
-import playerData from "../data/playerData";
 import { addWinsToPlayers, preparePlayerdata } from "../helpers/playerHelpers";
 
-
 const PlayerList = (props) => {
-  const playerDataArray = preparePlayerdata(playerData);
+  const playerDataArray = preparePlayerdata(props.playerData);
   const parsedPlayerData = addWinsToPlayers(playerDataArray, props.matchData);
   const onePlayer = parsedPlayerData[0]
-  // console.log(playerDataArray)
-  // console.log('parsed', parsedPlayerData)
-  
+
   return (
     <section className="PlayerList">
-    <h1>Current participating players</h1>
-    <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
-  </section>
+      <h1>Current participating players</h1>
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
+    </section>
   )
 }
 
