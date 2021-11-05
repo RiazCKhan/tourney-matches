46564d5b8b06cafa6c9b1117e86de7ed86f3d924
RiazK <RiazCKhan@gmail.com>
2021-11-05 11:22:05 -0400

    Pass playerData as prop to playerList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import MatchList from './components/MatchList';
 import PlayerList from './components/PlayerList';
 import matchData from './data/matchData';
+import playerData from './data/playerData';
 
 function App() {
   return (
     <div className="App">
       <h1>Tourney Matches<span>Where Coding and Tournaments found their Match!</span></h1>
-      <PlayerList matchData={ matchData } />
+      <PlayerList matchData={ matchData } playerData={ playerData } />
       <MatchList matchData={ matchData } />
     </div>
   );
