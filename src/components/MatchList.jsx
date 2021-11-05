341f0504266072668eb4c1ff16c036eb3d5143cb
RiazK <RiazCKhan@gmail.com>
2021-11-05 10:19:45 -0400

    Add prop passing to Match

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -2,12 +2,16 @@ import Match from "./Match";
 import matchData from "../data/matchData";
 
 const MatchList = (props) => {
+
+  const oneMatch = matchData[0];
+  // console.log('one match', oneMatch)
+
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match />
+      <Match player={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
-  )
+  );
 }
 
 export default MatchList;
